b10f3ed9761f1f2912bc588c0e866f301d668b65
Sharoz Ijaz <sherry@sharoz.dev>
2021-08-22 21:01:19 +0500

    Weather APP(V1)

---
+++ app.js
@@ -0,0 +1,73 @@
+"use strict";
+// Variables
+const searchCity = document.querySelector(".searchCity");
+const day = document.querySelector(".day");
+const date = document.querySelector(".date");
+const cityName = document.querySelector(".city-name");
+const feelLike = document.querySelector(".feels");
+const description = document.querySelector(".description");
+const weatherIcon = document.querySelector(".icon-weather");
+const temprature = document.querySelector(".weatherTemp-real");
+
+const key = "9cfa0bb4b1af700be136514df8902b9b";
+const Days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+searchCity.addEventListener(
+  "keypress",
+
+  function () {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${searchCity.value}&units=metric&APPID=${key}`
+    )
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (data) {
+        cityName.innerHTML = data.name;
+        feelLike.innerHTML = parseInt(data.main.feels_like);
+        temprature.innerHTML = parseInt(data.main.temp);
+        description.innerHTML = data.weather[0].description;
+        // Date Section
+        day.innerHTML = new Date().toLocaleString("en-GB", {
+          weekday: "long",
+        });
+        date.innerHTML = new Date()
+          .toLocaleDateString("en-GB", {
+            day: "numeric",
+            month: "short",
+            year: "numeric",
+          })
+          .replace(/ /g, "-");
+        weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        // data.weather[0].icon
+        //  http://openweathermap.org/img/wn/10d@2x.png
+        console.log(data);
+      })
+      .catch("Error");
+  }
+);
+
+// const getWeatherData = getWeatherData("london");
+// console.log(weather);
